ae304d80df3e293d08cea7e1559f2d43def88380
Alex Vandiver <alexmv@bestpractical.com>
2008-11-24 21:43:27 +0000

    r39490@kohr-ah:  chmrr | 2008-11-24 16:42:56 -0500   * First pass at CreateByProblemType

---
+++ lib/RT/Extension/CreateByProblemType.pm
@@ -0,0 +1,19 @@
+package RT::Extension::CreateByProblemType;
+
+our $VERSION = '0.01';
+
+use warnings;
+use strict;
+
+$RT::Config::META{CreateByProblemType} = 
+    {
+        Section         => 'General',
+        Overridable     => 1,
+        SortOrder       => 50,
+        Widget          => '/Widgets/Form/Boolean',
+        WidgetArguments => {
+            Description => 'Create tickets by selecting a problem type, not a queue',    #loc
+        },
+    };
+
+1;
